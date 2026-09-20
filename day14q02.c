2fb434c7c3f4db65ab3df8060decbab63a2fbc66
somvanshishubham653-ship-it <somvanshishubham653@gmail.com>
2026-09-21 02:21:31 +0530

    Add files via upload

---
+++ day14q02.c
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        sum += (2 * i - 1);
+    }
+
+    printf("Sum of the first %d odd numbers: %d\n", n, sum);
+    return 0;
+}
