386e90bb36d05e4a81f766470d089f9bd0d1365a
somvanshishubham653-ship-it <somvanshishubham653@gmail.com>
2026-09-21 01:47:15 +0530

    Add files via upload

---
+++ day03q02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    printf("After swapping: %d %d\n", num1, num2);
+
+    return 0;
+
